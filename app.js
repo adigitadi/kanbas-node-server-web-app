8b3e3378d8b03abd3f806f02796cf65615f35b98
Capt. Deadpool <yadav.ad@northeastern.edu>
2023-12-05 00:20:22 -0500

    update

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ import "dotenv/config";
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+const allowedOrigins = ['http://localhost:3000', 'https://a6--stirring-bombolone-0215ed.netlify.app'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
 const sessionOptions = {
   secret: 'any string',
   resave: false,
